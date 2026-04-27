64d7098c9788637f04d4520ac7db49986ef8e233
alec <alec@dhcp-10-250-145-242.harvard.edu>
2026-04-27 14:01:30 -0400

    q 6

--- analyze/spherical_harmonics.jl
+++ analyze/spherical_harmonics.jl
@@ -3,6 +3,7 @@ using LinearAlgebra
 using SpecialFunctions
 using LaTeXStrings
 using SphericalHarmonics # Probably should switch to using FastSphericalHarmonics at some point
+using Statistics
 
 readSliceXYZ(filepath) = (
     # function to read slice file and return array of particle positions
@@ -40,7 +41,7 @@ poss = poss ./ σ # nondimensionalize to radius
 neighbors = readNeighbors_gpt(neighbor_lists)
 
 XYZToSpherical(xyz) = (
-    # (x y z) -> (r θ ϕ)
+    # (x y z) -> (r θ ϕ) == (r polar azimuthal)
     sqrt(xyz⋅xyz) |>
         (r -> [r,
         acos(xyz[3]/r),
@@ -70,3 +71,41 @@ bonds = getBondList(neighbors,poss)
 
 # Sum Ylm
 # maybe figure out fastsphericalharmonics?
+
+function calculateQList(bond_list, l_max)
+    # r weighting?? Needs to drop off somehow
+    #=
+    Q_l = \sqrt{
+        4 pi / (2l + 1) *
+        \Sum_{-l}^l Magnitude(
+            (1/ N_bonds) *
+            \Sum_{i=1}^N_bonds(
+                Y_lm(\theta,\phi)
+            )
+        )^2
+    }
+    =#
+
+    N_b = size(bond_list, 1)
+
+    Ylms = [computeYlm(bond[2], bond[3], l_max) for bond in eachrow(bond_list)]
+    Q_ls = Array{Float64}(undef, l_max + 1)
+
+    for l in 0:l_max
+        sum_m = sum(-l:l) do m
+            avg_Ylm = sum(Ylm[(l, m)] for Ylm in Ylms) / N_b
+            abs2(avg_Ylm)
+        end
+
+        Q_ls[l + 1] = sqrt(4π / (2l + 1) * sum_m)
+    end
+
+    return Q_ls
+end
+
+Q_ls = [calculateQList(particle,8) for particle in bonds]
+
+Q_averages = mean(stack(Q_ls), dims=2)
+histogram([q[7] for q in Q_ls])
+xlabel!(L"Q_{l = 6}")
+ylabel!("Counts")
